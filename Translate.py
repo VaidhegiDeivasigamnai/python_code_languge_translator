e4a9192bc3600ede027ca9e627d9c54931b0092b
VaidhegiDeivasigamnai <86706248+VaidhegiDeivasigamnai@users.noreply.github.com>
2023-04-13 11:20:22 +1000

    Add files via upload

---
+++ Translate.py
@@ -0,0 +1,76 @@
+from tkinter import *
+from tkinter import ttk
+from tkinter.ttk import *
+import tkinter
+import tkinter.ttk
+import googletrans
+import textblob
+import tkinter.messagebox
+
+
+translate = Tk()
+translate.title("Simple Translator")
+translate.geometry("830x300")
+translate.configure(bg="#9A32CD")
+
+def click():
+    
+    translated_text.delete(1.0, END)
+
+    try:
+       
+        #get languages from language key
+        for key, value in languages.items():
+            if (value == actual_text.get()):
+                from_language_key = key
+        
+        for key, value in languages.items():
+            if (value == translated_text.get()):
+                to_language_key = key
+
+        #converrt to textblob
+        text = textblob.Textblob(actual_text.get(1.0, END))
+
+        #translate command
+        text = text.translate(from_Lang=from_language_key , to=to_language_key)
+
+        #translate output
+        translated_text.insert(1.0, text)
+
+    except Exception as e:
+        tkinter.messagebox.messagebox.showerror("Language error", e)
+
+            #google translate
+languages = googletrans.LANGUAGES
+
+language_list = list(languages.values())
+
+
+#text box
+actual_text = Text(translate, height=8, width=32)
+actual_text.grid(row=0, column=0)
+actual_text.grid(padx=20, pady=20)
+actual_text.configure(bg="#FFEBCD")
+
+translate_button = Button(translate, text="Translate")
+translate_button.grid(row=0, column=1, padx=10)
+
+translated_text = Text(translate, height=8, width=32)
+translated_text.grid(row=0, column=2)
+translated_text.grid(padx=20, pady=20)
+translated_text.configure(bg="#FFEBCD")
+
+#language
+default_language = ttk.Combobox(translate, width=50, value=language_list)
+default_language.current(21)
+default_language.grid(row=1, column=0)
+
+translated_language = ttk.Combobox(translate, width=50, value=language_list)
+translated_language.current(26)
+translated_language.grid(row=1, column=2)
+
+translate.mainloop()
+
+
+
+
